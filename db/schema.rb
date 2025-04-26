a2eb13fd1916ea80d6075e31601d58494b9c0f47
henrique vera <hverar@gmail.com>
2025-04-26 11:40:37 -0400

    cleaned up errors in migration, and added photos on the tool index page

--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.1].define(version: 2025_04_25_005609) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tools", force: :cascade do |t|
-
-    t.string "name"
-    t.text "description"
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "price"
+    t.string "condition"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image_url"
     t.decimal "tool_price", precision: 8, scale: 2
-    t.string "condition"
     t.bigint "owner_id"
+    t.string "image_url"
     t.index ["owner_id"], name: "index_tools_on_owner_id"
-
+    t.index ["user_id"], name: "index_tools_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,6 +43,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_25_005609) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tools", "users"
   add_foreign_key "tools", "users", column: "owner_id"
-
 end
