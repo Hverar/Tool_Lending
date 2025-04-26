b9d1020bed8674f937c153def0318c69b310c85e
henrique vera <hverar@gmail.com>
2025-04-26 10:41:01 -0400

    try to fix error in migration

--- db/migrate/20250425004020_add_fields_to_tools.rb
+++ db/migrate/20250425004020_add_fields_to_tools.rb
@@ -1,7 +1,6 @@
 class AddFieldsToTools < ActiveRecord::Migration[7.1]
   def change
     add_column :tools, :tool_price, :decimal, precision: 8, scale: 2
-    add_column :tools, :condition, :string
     add_reference :tools, :owner, foreign_key: { to_table: :users }, null: true
   end
 end
