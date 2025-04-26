1302f8d474fb7bfc6300ee1b6c05d13d6131e16a
AnastasiaRoussakis <a_roussakis@outlook.com>
2025-04-26 13:51:17 -0400

    Finished the create file where it will now post the new tool listing

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,6 +1,5 @@
 class ToolsController < ApplicationController
-
-  before_action :set_tool, only: [:show, :create]
+  before_action :set_tool, only: [:show]
 
   def new
     @tool = Tool.new
@@ -8,18 +7,18 @@ class ToolsController < ApplicationController
 
   def create
     @tool = Tool.new(tool_params)
-    @tool.user = current_user # assuming Devise or similar
+    @tool.user = current_user # or however you're setting the user
     if @tool.save
-      redirect_to tool_path(@tool), notice: "Tool listed successfully!"
+      redirect_to tools_path, notice: "Tool listed successfully!"
     else
       render :new, status: :unprocessable_entity
     end
   end
-  
+
  def index
    @tools = Tool.all
  end
-  
+
   def show
   end
 
@@ -30,6 +29,6 @@ class ToolsController < ApplicationController
   end
 
   def tool_params
-    params.require(:tool).permit(:name, :brand, :condition, :price)
+    params.require(:tool).permit(:name, :description, :condition, :price, :image)
   end
 end
