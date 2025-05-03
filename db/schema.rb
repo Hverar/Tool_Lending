ab573a8ed2accd5d92a0afc0c33d28ff6ca10512
AnastasiaRoussakis <a_roussakis@outlook.com>
2025-05-03 17:30:26 -0400

    Finalized look of website

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.1].define(version: 2025_05_03_195634) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -91,7 +89,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_03_195634) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
-    t.string "role"
+    t.string "role", default: "consumer"
     t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
