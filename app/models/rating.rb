cc91ab741e26de3375be621e76f1355be7f78a39
henrique vera <hverar@gmail.com>
2025-05-03 17:14:05 -0400

    star systme cleaned

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,5 +3,20 @@ class Rating < ApplicationRecord
   belongs_to :tool
 
   validates :score, presence: true, inclusion: { in: 1..5 }
-  # validates :user_id, uniqueness: { scope: :tool_id, message: "can only rate once per tool" }
+  validates :user_id, uniqueness: { scope: :tool_id, message: "can only rate once per tool" }
+  validate :must_have_accepted_booking
+
+  private
+
+  def must_have_accepted_booking
+    valid_booking = Booking.exists?(
+      user_id: user_id,
+      tool_id: tool_id,
+      status: "accepted"
+    )
+
+    unless valid_booking
+      errors.add(:base, "You can only rate a tool after your booking has been accepted.")
+    end
+  end
 end
