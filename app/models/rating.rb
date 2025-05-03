ba8134580a305954af1b0edd830a6c03e7ce2bf0
henrique vera <hverar@gmail.com>
2025-05-03 16:46:18 -0400

    herokue

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,5 +3,5 @@ class Rating < ApplicationRecord
   belongs_to :tool
 
   validates :score, presence: true, inclusion: { in: 1..5 }
-  # validates :user_id, uniqueness: { scope: :tool_id, message: "can only rate once per tool" }
+  validates :user_id, uniqueness: { scope: :tool_id, message: "can only rate once per tool" }
 end
